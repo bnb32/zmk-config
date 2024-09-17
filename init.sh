69cdf625bac3b8d5649fe7bf81692f4969074f61
bnb32 <brandon.benton@nrel.gov>
2024-09-17 16:12:39 +0000

    init script for docker setup and docket container configs

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bash <(curl -L https://nixos.org/nix/install) --no-daemon
+source '/home/$USER/.nix-profile/etc/profile.d/nix.sh'
+nix profile install nixpkgs#direnv nixpkgs#nix-direnv --extra-experimental-features nix-command --extra-experimental-features flakes
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+mkdir -p ~/.config/direnv
+echo 'source $HOME/.nix-profile/share/nix-direnv/direnvrc' >> ~/.config/direnv/direnvrc
+source ~/.bashrc
+
+cd '/workspaces/zmk-config'
+direnv allow
+just init
