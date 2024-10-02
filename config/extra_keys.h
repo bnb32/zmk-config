4fbd7446ee35c69153e815101d7276c709ab39a1
bnb32 <brandon.benton@nrel.gov>
2024-10-02 14:54:53 +0000

    removed some redundant code and just added more args for different time values to MAKE_HOLD_TAP and MAKE_TAP_DANCE to use for nav and general applications

--- config/extra_keys.h
+++ config/extra_keys.h
@@ -1,18 +1,18 @@
 // The default layout has 42 keys. Additional keys can be added by pre-setting any of
 // the macros defined in this file to one or more keys before sourcing this file.
 
-#if !defined X_TOP // top row for solfe
+#if !defined X_TOP // top row for solfe - this includes 12 keys
     #define X_TOP
 #endif
 
-#if !defined X_RT // extra right thumb keys for solfe
+#if !defined X_RT // extra right thumb keys for solfe - this includes 2 keys
     #define X_RT
 #endif
 
-#if !defined X_LT // extra left thumb keys for solfe
+#if !defined X_LT // extra left thumb keys for solfe - this includes 2 keys
     #define X_LT
 #endif
 
-#if !defined X_ENC // extra encoder knobs
+#if !defined X_ENC // extra encoder knobs - this includes left and right knobs
     #define X_ENC
 #endif
