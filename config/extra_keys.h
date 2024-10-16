6abb69257bd08c7e0cdf8bd7ace6bd15a275fb29
bnb32 <brandon.benton@nrel.gov>
2024-10-16 11:46:13 +0000

    decomposed extra top row for sofle and lily into row for default layer and row for non default layer

--- config/extra_keys.h
+++ config/extra_keys.h
@@ -1,8 +1,12 @@
 // The default layout has 42 keys. Additional keys can be added by pre-setting any of
 // the macros defined in this file to one or more keys before sourcing this file.
 
-#if !defined X_TOP // top row for solfe - this includes 12 keys
-    #define X_TOP
+#if !defined DEF_X_TOP // top row for solfe / lily for the default layer - this includes 12 keys
+    #define DEF_X_TOP
+#endif
+
+#if !defined NON_DEF_X_TOP // top row for solfe / lily for non default layers - this includes 12 keys
+    #define NON_DEF_X_TOP
 #endif
 
 #if !defined X_RT // extra right thumb keys for solfe - this includes 2 keys
